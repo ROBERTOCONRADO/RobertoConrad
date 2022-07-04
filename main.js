750e5c6732699986a2bb856651c11f261bd43e55
RobertoConrado <robertoconradorc@gmail.com>
2022-07-04 11:01:17 -0300

    ScrollReveal

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
+    distance: '150px',
     duration: 2000,
     reset: true,
 });
