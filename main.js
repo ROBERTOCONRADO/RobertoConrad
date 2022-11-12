e7bdc8440463975502dbe7d0befa2b3a800f10de
Roberto Conrado <robertoconradorc@gmail.com>
2022-11-12 14:42:17 -0300

    upgrade-curriculum

--- main.js
+++ main.js
@@ -1,68 +1,63 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+showMenu('nav-toggle', 'nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
   /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
+  navLink.forEach((n) => n.classList.remove('active'));
   this.classList.add('active');
-  
+
   /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '20px',
-    duration: 2000,
-    reset: false
+  origin: 'top',
+  distance: '20px',
+  duration: 2000,
+  reset: false,
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 400 });
+sr.reveal('.about__text', { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 400}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 400 });
+sr.reveal('.skills__img', { delay: 600 });
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 400}); 
+sr.reveal('.work__img', { interval: 400 });
 
 /* SCROLL MyWork*/
-sr.reveal('.my__work__img',{interval: 400});
+sr.reveal('.my__work__img', { interval: 400 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 400});
+sr.reveal('.contact__input', { interval: 400 });
 
-let Roberto = document.querySelector('.nav__logo').style.animation = 'slidein 2s';
-
-
-
-
-
-
+let Roberto = (document.querySelector('.nav__logo').style.animation =
+  'slidein 2s');
