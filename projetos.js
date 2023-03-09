d05e34fd59eaa9a5e0b7527e689a36fea5162040
Roberto Conrado <robertoconradorc@gmail.com>
2023-03-08 22:06:23 -0300

    js

---
+++ projetos.js
@@ -0,0 +1,70 @@
+let projeto1 = document.querySelector(".projeto1");
+let projeto2 = document.querySelector(".projeto2");
+let projeto3 = document.querySelector(".projeto3");
+let projeto4 = document.querySelector(".projeto4");
+let projeto5 = document.querySelector(".projeto5");
+let projeto6 = document.querySelector(".projeto6");
+let projeto7 = document.querySelector(".projeto7");
+let projeto8 = document.querySelector(".projeto8");
+let projeto9 = document.querySelector(".projeto9");
+let projeto10 = document.querySelector(".projeto10");
+let projeto11 = document.querySelector(".projeto11");
+let projeto12 = document.querySelector(".projeto12");
+
+projeto1.addEventListener("mouseover", function() {
+  projeto2.style.display = "none";
+  document.querySelector(".right1").style.display = "block";
+});
+projeto1.addEventListener("mouseout", function() {
+  projeto2.style.display = "block";
+  document.querySelector(".right1").style.display = "none";
+});
+projeto2.addEventListener("mouseover", function() {
+  projeto1.style.display = "none";
+  document.querySelector(".left1").style.display = "block";
+});
+projeto2.addEventListener("mouseout", function() {
+  projeto1.style.display = "block";
+  document.querySelector(".left1").style.display = "none";
+});
+
+
+
+projeto3.addEventListener("mouseover", function() {
+  projeto4.style.display = "none";
+  document.querySelector(".right2").style.display = "block";
+});
+projeto3.addEventListener("mouseout", function() {
+  projeto4.style.display = "block";
+  document.querySelector(".right2").style.display = "none";
+});
+projeto4.addEventListener("mouseover", function() {
+  projeto3.style.display = "none";
+  document.querySelector(".left2").style.display = "block";
+});
+projeto4.addEventListener("mouseout", function() {
+  projeto3.style.display = "block";
+  document.querySelector(".left2").style.display = "none";
+});
+
+
+projeto1.addEventListener("mouseover", function() {
+  projeto2.style.display = "none";
+  document.querySelector(".right1").style.display = "block";
+});
+projeto1.addEventListener("mouseout", function() {
+  projeto2.style.display = "block";
+  document.querySelector(".right1").style.display = "none";
+});
+
+projeto1.addEventListener("mouseover", function() {
+  projeto2.style.display = "none";
+  document.querySelector(".right1").style.display = "block";
+});
+projeto1.addEventListener("mouseout", function() {
+  projeto2.style.display = "block";
+  document.querySelector(".right1").style.display = "none";
+});
+
+
+
